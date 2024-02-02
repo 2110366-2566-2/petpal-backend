dfa80eb647c4754acd8c9c3c05cc63ef044bd129
jirayuwat12 <jirayuwat12@gmail.com>
2024-02-02 22:43:38 +0700

    Update mocker.py and add svcp_schema.json

--- mock/mocker.py
+++ mock/mocker.py
@@ -8,8 +8,8 @@ from pymongo.server_api import ServerApi
 
 # Config this part
 CLEAR_PREV = True # clear the collection before inserting new records
-N = 1000 # number of records to generate
-COLLECTION_NAME = 'user' # collection name
+N = 500 # number of records to generate
+COLLECTION_NAMES = ['user', 'svcp'] # collection name
 
 # Connect to MongoDB
 USERNAME = 'inwza'
@@ -28,18 +28,16 @@ except Exception as e:
 
 db = client["petpal"]
 if CLEAR_PREV:
-    db[COLLECTION_NAME].drop()
-    print(f"Collection {COLLECTION_NAME} dropped")
+    for collection_name in COLLECTION_NAMES:
+        if collection_name not in db.list_collection_names():
+            print(f"Collection {collection_name} not found")
+            continue
+        db[collection_name].drop()
+        print(f"Collection {collection_name} dropped")
 
-user_collection = db[COLLECTION_NAME]
 
-print(f"Database : petpal\nCollection : {COLLECTION_NAME}")
+print(f"Database : petpal")
 
-# read schema json file
-schema_path = os.path.abspath(f'./mock/{COLLECTION_NAME}_schema.json')
-with open(schema_path) as file:
-    print(f"Reading schema from {schema_path}")
-    user_schema = json.load(file)
 
 def gen_object(id, properties_dict):
     ret = dict()
@@ -47,36 +45,54 @@ def gen_object(id, properties_dict):
     for key in properties_dict:
         match properties_dict[key]['bsonType']:
             case 'string':
-                if 'date' in key: # date special case
-                    ret[key] = f"{random.randint(1, 30)}/{random.randint(1, 12)}/{random.randint(1900, 2021)}"
-                else: # normal string
-                    if 'pattern' in properties_dict[key]:
-                        limit = 20 if 'maxLength' not in properties_dict[key] else properties_dict[key]['maxLength']
-                        ret[key] = exrex.getone(properties_dict[key]['pattern'], int(limit))
-                    else:
-                        ret[key] = key+str(id)
+                if 'pattern' in properties_dict[key]:
+                    limit = 20 if 'maxLength' not in properties_dict[key] else properties_dict[key]['maxLength']
+                    ret[key] = exrex.getone(properties_dict[key]['pattern'], int(limit))
+                else:
+                    ret[key] = key+str(id)
+                if 'minLength' in properties_dict[key] and len(ret[key]) < properties_dict[key]['minLength']:
+                    ret[key] = ret[key] + "0"*(properties_dict[key]['minLength']-len(ret[key]))
 
-                    if 'minLength' in properties_dict[key] and len(ret[key]) < properties_dict[key]['minLength']:
-                        ret[key] = ret[key] + "0"*(properties_dict[key]['minLength']-len(ret[key]))
-            case 'int':
+            case 'date':
+                ret[key] = f"{random.randint(1, 30)}/{random.randint(1, 12)}/{random.randint(1900, 2021)}"
+
+            case 'int' | 'double':
                 minimum = 0 if 'minimum' not in properties_dict[key] else properties_dict[key]['minimum']
                 maximum = 100 if 'maximum' not in properties_dict[key] else properties_dict[key]['maximum']
-                ret[key] = random.randint(minimum, maximum)
+                if properties_dict[key]['bsonType'] == 'int':
+                    ret[key] = random.randint(minimum, maximum)
+                elif properties_dict[key]['bsonType'] == 'double':
+                    ret[key] = random.uniform(minimum, maximum)
+
+            case 'bool':
+                ret[key] = random.choice([True, False])
+
             case 'array':
                 arr = []
                 for i in range(random.randint(1, 6)):
                     arr.append(gen_object(1, properties_dict[key]['items']['properties']))
                 ret[key] = arr
+
             case _:
-                print('error : '+properties_dict[key]['bsonType'])
+                print('error (type='+properties_dict[key]['bsonType']+') not implemented')
     
     return ret
 
-print(f"Generating {N} records")
-for i in range(N):
-    rec = gen_object(i, user_schema['properties'])
-    user_collection.insert_one(rec)
-    if i%10 == 0:
-        print('.', end='')
+for idx, collection_name in enumerate(COLLECTION_NAMES):
+    print(f"Mocking collection {collection_name}")
+    collection = db[collection_name]
+    
+    schema_path = os.path.abspath(f'./mock/{collection_name}_schema.json')
+    with open(schema_path) as file:
+        print(f"\tReading schema from {schema_path}")
+        user_schema = json.load(file)
+    
+    iter = N if isinstance(N, int) else N[idx]
+    print(f"\tGenerating {iter} records -> ", end='')
+    for i in range(iter):
+        rec = gen_object(i, user_schema['properties'])
+        collection.insert_one(rec)
+        if i%10 == 0:
+            print('.', end='')
 
-print("\nDone all")
+    print(f"\n\tDone {collection_name}")
