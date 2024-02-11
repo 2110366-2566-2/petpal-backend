7a8a8b05f77d057d0faf7f4582c66c7e74af5cb3
Pakin Kraisin <dxpakin@gmail.com>
2024-02-11 11:27:47 +0700

    fix: :bug: add tag bson to match data in database

--- src/models/svcp.entity.go
+++ src/models/svcp.entity.go
@@ -4,18 +4,18 @@ package models
 // User represents a user entity
 type SVCP struct {
 	Individual
-	SVCPID                string `json:"SVCPID"`
-	SVCPImg               string `json:"SVCPImg"`
-	SVCPUsername          string `json:"SVCPUsername"`
-	SVCPPassword          string `json:"SVCPPassword"`
-	SVCPEmail             string `json:"SVCPEmail"`
+	SVCPID                string `json:"SVCPID" bson:"SVCPID"`
+	SVCPImg               string `json:"SVCPImg" bson:"SVCPImg"`
+	SVCPUsername          string `json:"SVCPUsername" bson:"SVCPUsername"`
+	SVCPPassword          string `json:"SVCPPassword" bson:"SVCPPassword"`
+	SVCPEmail             string `json:"SVCPEmail" bson:"SVCPEmail"`
 	isVerified            bool   `json:"isVerified"`
-	SVCPResponsiblePerson string `json:"SVCPResponsiblePerson"`
+	SVCPResponsiblePerson string `json:"SVCPResponsiblePerson" bson:"SVCPResponsiblePerson"`
 	defaultBank           string `json:"defaultBank"`
 	defaultAccountNumber  string `json:"defaultAccountNumber"`
 	license               string `json:"license"`
 	location              string `json:"location"`
-	SVCPAdditionalImg     string `json:"SVCPAdditionalImg"`
+	SVCPAdditionalImg     string `json:"SVCPAdditionalImg" bson:"SVCPAdditionalImg"`
 }
 
 func (e *SVCP) validate(SVCPImg string) bool {
