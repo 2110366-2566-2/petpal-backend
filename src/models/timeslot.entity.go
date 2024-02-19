09566c941d1584887fca6fe477a785a87bc8f386
jirayuwat12 <jirayuwat12@gmail.com>
2024-02-19 19:36:29 +0700

    feat(schema): :sparkles: update SVCP, Service to handle booking and feedback

--- src/models/timeslot.entity.go
+++ src/models/timeslot.entity.go
@@ -5,6 +5,7 @@ import "time"
 
 // User represents a user entity
 type Timeslot struct {
+	TimeslotID  string    `json:"timeslotID" bson:"timeslotID"`
 	StartTime   time.Time `json:"startTime" bson:"startTime"`
 	EndTime     time.Time `json:"endTime" bson:"endTime"`
 }
