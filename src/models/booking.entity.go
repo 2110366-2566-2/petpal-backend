382525c42a003187230415dcaba8e273f71c3eb5
supakorn22 <6432173821@student.chula.ac.th>
2024-03-01 07:37:30 +0700

    feat: :sparkles: change booking shema and add booking struct

--- src/models/booking.entity.go
+++ src/models/booking.entity.go
@@ -27,6 +27,53 @@ type BookingWithId struct {
 	Feedback          Feedback      `json:"feedback" bson:"feedback"`
 }
 
+type BookingShowHistory struct {
+	BookingID  string `json:"bookingID" bson:"_id"`
+	UserID     string `json:"userID" bson:"userID"`
+	SVCPID     string `json:"SVCPID" bson:"SVCPID"`
+	ServiceID  string `json:"serviceID" bson:"serviceID"`
+	TimeslotID string `json:"timeslotID" bson:"timeslotID"`
+
+	BookingTimestamp  time.Time `json:"bookingTimestamp" bson:"bookingTimestamp"`
+	TotalBookingPrice float64   `json:"totalBookingPrice" bson:"totalBookingPrice"`
+
+	ServiceName string    `json:"serviceName" bson:"serviceName"`
+	SVCPName    string    `json:"SVCPName" bson:"SVCPName"`
+	StartTime   time.Time `json:"startTime" bson:"startTime"`
+	EndTime     time.Time `json:"endTime" bson:"endTime"`
+
+	BookingStatus BookingStatus `json:"bookingStatus" bson:"bookingStatus"`
+}
+
+type BookingFull struct {
+	BookingID  string `json:"bookingID" bson:"_id"`
+	UserID     string `json:"userID" bson:"userID"`
+	SVCPID     string `json:"SVCPID" bson:"SVCPID"`
+	ServiceID  string `json:"serviceID" bson:"serviceID"`
+	TimeslotID string `json:"timeslotID" bson:"timeslotID"`
+
+	BookingTimestamp  time.Time `json:"bookingTimestamp" bson:"bookingTimestamp"`
+	TotalBookingPrice float64   `json:"totalBookingPrice" bson:"totalBookingPrice"`
+
+	Service Service `json:"service" bson:"service"`
+
+	StartTime time.Time `json:"startTime" bson:"startTime"`
+	EndTime   time.Time `json:"endTime" bson:"endTime"`
+
+	BookingStatus BookingStatus `json:"bookingStatus" bson:"bookingStatus"`
+
+	PaymentStatus          bool      `json:"paymentStatus" bson:"paymentStatus"`
+	PaymentTimestamp       time.Time `json:"paymentTimestamp" bson:"paymentTimestamp"`
+	SvcpConfirmed          bool      `json:"svcpConfirmed" bson:"svcpConfirmed"`
+	SvcpConfirmedTimestamp time.Time `json:"svcpConfirmedTimestamp" bson:"svcpConfirmedTimestamp"`
+	SvcpCompleted          bool      `json:"svcpCompleted" bson:"svcpCompleted"`
+	SvcpCompletedTimestamp time.Time `json:"svcpCompletedTimestamp" bson:"svcpCompletedTimestamp"`
+	UserCompleted          bool      `json:"userCompleted" bson:"userCompleted"`
+	UserCompletedTimestamp time.Time `json:"userCompletedTimestamp" bson:"userCompletedTimestamp"`
+
+	Feedback Feedback `json:"feedback" bson:"feedback"`
+}
+
 type BookingRequest struct {
 	SVCPID     string `json:"SVCPID" bson:"SVCPID"`
 	ServiceID  string `json:"serviceID" bson:"serviceID"`
@@ -43,16 +90,17 @@ type BookingIndex struct {
 type BookingStatus string
 
 const (
-	BookingPending   BookingStatus = "pending payment"            //waiting for user to pay
-	BookingPaid      BookingStatus = "payment paid"               //user has paid
-	BookingComfirmed BookingStatus = "service provided comfirmed" //svcp has confirmed waiting for user to pay
-	BookingCompleted BookingStatus = "completed"                  //service has been provided
+	BookingPending   BookingStatus = "pending payment"            //waiting for user to pay 					1
+	BookingPaid      BookingStatus = "payment paid"               //user has paid wait svcp comfirm				2 or 3
+	BookingComfirmed BookingStatus = "service provided comfirmed" //svcp has confirmed waiting for user to pay 	3 or 2
+	BookingCompleted BookingStatus = "completed"                  //service has been provided					4
 
 	BookingCanceledUser BookingStatus = "cancelled by user"             //user has cancelled
 	BookingCanceledSvcp BookingStatus = "cancelled by service provider" //svcp has cancelled
 
 	//BookingRescheduled BookingStatus = "rescheduled" //user has rescheduled
 
+	// may be un used
 	BookingExpiredPaid      BookingStatus = "expired from unpaid"                                //user has not paid in time
 	BookingExpiredComfirmed BookingStatus = "expired from pending service provider confirmation" //svcp has not confirmed in time
 )
