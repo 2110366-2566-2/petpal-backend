26503ef22e9f40a35b00428844a9c10a4a925b4a
Pakin Kraisin <dxpakin@gmail.com>
2024-03-13 01:04:35 +0700

    feat: :sparkles: add booking notification email sending utils
    create booking, reschedule booking, confirm, cancels, completes notification emails

---
+++ src/utills/service/service.booking.notification.utills.go
@@ -0,0 +1,170 @@
+package utills
+
+import (
+	"fmt"
+	"petpal-backend/src/models"
+	mail "petpal-backend/src/utills/email"
+)
+
+// User create booking
+func NotifyCreateBooking(booking *models.Booking, user *models.User, svcp *models.SVCP) error {
+	emailSubject := "Petpal service booking confirmed"
+	emailContent := fmt.Sprintf(`
+		<h4>สวัสดีครับ</h4>
+		<p>ผู้ใช้บริการ %s<br>
+		ได้สร้างคำขอการจองบริการ %s<br>
+		ในเวลา %s ถึงเวลา %s<\p>`,
+		user.FullName,
+		booking.ServiceName,
+		booking.StartTime.Format("15:04:05 02-01-2006 MST"),
+		booking.EndTime.Format("15:04:05 02-01-2006 MST"),
+	)
+
+	emailContent += `<p>โปรดตรวจสอบความถูกต้องของการจอง แล้วยืนยันการจองในระบบ</p><p>ขอขอบคุณที่ใช้บริการกับเรา</p><p>Petpal Team</p>`
+	err := mail.SendEmailWithGmail(svcp.SVCPEmail, emailSubject, emailContent)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// User reschedule booking
+func NotifyRescheduleBooking(booking *models.Booking, user *models.User, svcp *models.SVCP) error {
+	emailSubject := "Petpal service booking scheduled changed"
+	emailContent := fmt.Sprintf(`
+		<h4>สวัสดีครับ</h4>
+		<p>ผู้ใช้บริการ %s<br>
+		ได้เปลี่ยนเวลาการจองบริการ %s<br>
+		เป็นเวลา %s ถึงเวลา %s<\p>`,
+		user.FullName,
+		booking.ServiceName,
+		booking.StartTime.Format("15:04:05 02-01-2006 MST"),
+		booking.EndTime.Format("15:04:05 02-01-2006 MST"),
+	)
+
+	emailContent += `<p>โปรดตรวจสอบความถูกต้องของการจอง แล้วยืนยันการจองในระบบ</p><p>ขอขอบคุณที่ใช้บริการกับเรา</p><p>Petpal Team</p>`
+	err := mail.SendEmailWithGmail(svcp.SVCPEmail, emailSubject, emailContent)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// SVCP confirm booking
+func NotifyConfirmBookingToUser(booking *models.Booking, user *models.User, svcp *models.SVCP) error {
+	emailSubject := "Petpal service booking confirmed"
+	emailContent := fmt.Sprintf(`
+		<h4>สวัสดีครับ</h4>
+		<p>ผู้ให้บริการ %s<br>
+		ได้ยืนยันการจองบริการ %s<br>
+		ในเวลา %s ถึงเวลา %s<br>
+		เรียบร้อยแล้ว<\p>`,
+		svcp.SVCPUsername,
+		booking.ServiceName,
+		booking.StartTime.Format("15:04:05 02-01-2006 MST"),
+		booking.EndTime.Format("15:04:05 02-01-2006 MST"),
+	)
+
+	emailContent += `<p>ขอขอบคุณที่ใช้บริการกับเรา</p><p>Petpal Team</p>`
+	err := mail.SendEmailWithGmail(user.Email, emailSubject, emailContent)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Cancel booking
+func NotifyCancelBookingToUser(booking *models.Booking, user *models.User, svcp *models.SVCP) error {
+	emailSubject := "Petpal service booking cancelled"
+	emailContent := fmt.Sprintf(`
+		<h4>สวัสดีครับ</h4>
+		<p>การจองบริการ %s<br>
+		กับผู้ให้บริการ %s<br>
+		เวลา %s ถึงเวลา %s<br>
+		ได้ถูกยกเลิกแล้ว`,
+		booking.ServiceName,
+		svcp.SVCPUsername,
+		booking.StartTime.Format("15:04:05 02-01-2006 MST"),
+		booking.EndTime.Format("15:04:05 02-01-2006 MST"),
+	)
+	if booking.Cancel.CancelReason != "" {
+		emailContent += fmt.Sprintf(`<br>ด้วยสาเหตุ %s`, booking.Cancel.CancelReason)
+	}
+	emailContent += `</p><p>ขออภัยในความไม่สะดวก</p><p>Petpal Team</p>`
+
+	err := mail.SendEmailWithGmail(user.Email, emailSubject, emailContent)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NotifyCancelBookingToSVCP(booking *models.Booking, user *models.User, svcp *models.SVCP) error {
+	emailSubject := "Petpal service booking cancelled"
+	emailContent := fmt.Sprintf(`
+		<h4>สวัสดีครับ</h4>
+		<p>การจองบริการ %s<br>
+		กับผู้ใช้บริการ %s<br>
+		เวลา %s ถึงเวลา %s<br>
+		ได้ถูกยกเลิกแล้ว`,
+		booking.ServiceName,
+		user.FullName,
+		booking.StartTime.Format("15:04:05 02-01-2006 MST"),
+		booking.EndTime.Format("15:04:05 02-01-2006 MST"),
+	)
+	if booking.Cancel.CancelReason != "" {
+		emailContent += fmt.Sprintf(`<br>ด้วยสาเหตุ %s`, booking.Cancel.CancelReason)
+	}
+	emailContent += `</p><p>ขออภัยในความไม่สะดวก</p><p>Petpal Team</p>`
+
+	err := mail.SendEmailWithGmail(svcp.SVCPEmail, emailSubject, emailContent)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Complete booking
+func NotifyCompleteBookingToUser(booking *models.Booking, user *models.User, svcp *models.SVCP) error {
+	emailSubject := "Petpal service booking marked as completed"
+	emailContent := fmt.Sprintf(`
+		<h4>สวัสดีครับ</h4>
+		<p>ผู้ให้บริการ %s<br>
+		ได้ยืนยันการจองบริการ %s<br>
+		เวลา %s ถึงเวลา %s<br>
+		ว่าสำเร็จแล้ว`,
+		booking.ServiceName,
+		svcp.SVCPUsername,
+		booking.StartTime.Format("15:04:05 02-01-2006 MST"),
+		booking.EndTime.Format("15:04:05 02-01-2006 MST"),
+	)
+	emailContent += `</p><p>ขอขอบคุณที่ใช้บริการกับเรา</p><p>Petpal Team</p>`
+
+	err := mail.SendEmailWithGmail(user.Email, emailSubject, emailContent)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NotifyCompleteBookingToSVCP(booking *models.Booking, user *models.User, svcp *models.SVCP) error {
+	emailSubject := "Petpal service booking marked as completed"
+	emailContent := fmt.Sprintf(`
+		<h4>สวัสดีครับ</h4>
+		<p>ผู้ใช้บริการ %s<br>
+		ได้ยืนยันการจองบริการ %s<br>
+		เวลา %s ถึงเวลา %s<br>
+		ว่าสำเร็จแล้ว`,
+		user.FullName,
+		booking.ServiceName,
+		booking.StartTime.Format("15:04:05 02-01-2006 MST"),
+		booking.EndTime.Format("15:04:05 02-01-2006 MST"),
+	)
+	emailContent += `</p><p>ขอขอบคุณที่ใช้บริการกับเรา</p><p>Petpal Team</p>`
+	err := mail.SendEmailWithGmail(svcp.SVCPEmail, emailSubject, emailContent)
+	if err != nil {
+		return err
+	}
+	return nil
+}
