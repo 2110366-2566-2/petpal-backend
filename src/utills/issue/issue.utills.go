1263ecf016d7096834c57282dd32a45cd04d31de
Pakin Kraisin <67536742+differentail@users.noreply.github.com>
2024-04-05 21:06:23 +0700

    feat: :sparkles: fix objectid and createissue

--- src/utills/issue/issue.utills.go
+++ src/utills/issue/issue.utills.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"petpal-backend/src/models"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -13,7 +14,20 @@ import (
 func CreateIssue(db *models.MongoDB, issue *models.CreateIssue) error {
 	collection := db.Collection("issue")
 
-	_, err := collection.InsertOne(context.Background(), issue)
+	new_issue := models.Issue{
+		IssueID:             primitive.NewObjectID().Hex(),
+		IssueDate:           time.Now(),
+		IsResolved:          false,
+		WorkingAdminID:      "",
+		ReporterID:          issue.ReporterID,
+		ReporterType:        issue.ReporterType,
+		Details:             issue.Details,
+		AttachedImg:         issue.AttachedImg,
+		IssueType:           issue.IssueType,
+		AssociatedBookingID: issue.AssociatedBookingID,
+	}
+
+	_, err := collection.InsertOne(context.Background(), new_issue)
 	if err != nil {
 		return err
 	}
@@ -43,8 +57,7 @@ func GetIssues(db *models.MongoDB, filter bson.M, page int64, per int64) ([]mode
 func AdminAcceptIssue(db *models.MongoDB, issueID string, adminID string) error {
 	collection := db.Collection("issue")
 
-	oid, _ := primitive.ObjectIDFromHex(issueID)
-	filter := bson.M{"_id": oid}
+	filter := bson.M{"_id": issueID}
 	update := bson.M{"$set": bson.M{"workingAdminID": adminID}}
 
 	result, err := collection.UpdateOne(context.Background(), filter, update)
